524bd33aacfe9ab83cd536efdf01a1d83e334a1a
Harsha0111 <selviharsha@gmail.com>
2022-08-12 18:00:00 +0530

    day02

---
+++ day02/CALCULATOR/src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) {
+        Cal cal = new Cal();
+        cal.add();
+
+        int sum = cal.add();
+        System.out.println(sum);
+
+        System.out.println(cal.add());
+
+        Cal sasas = new Cal();
+    }
+}
---
+++ day02/CALCULATOR/src/main/java/Cal.java
@@ -0,0 +1,14 @@
+public class Cal {
+    private int a;
+    private int b;
+
+    public Cal() {
+        this.a = 3;
+        this.b = 5;
+    }
+
+
+    int add() {
+        return a + b;
+    }
+}
