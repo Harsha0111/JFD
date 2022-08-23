a8e9dd210ddfc444eade8706d455e98df9d54f2b
Harsha0111 <selviharsha@gmail.com>
2022-08-23 18:02:34 +0530

    with args without args

--- day09/Basics/src/main/java/Application.java
+++ day09/Basics/src/main/java/Application.java
@@ -1,7 +1,11 @@
 public class Application {
     public static void main(String[] args) {
-        Add add = new Add();
-        add.add();
-
+        Calculator calculator = new Calci();
+        int resAdd = calculator.add();
+        System.out.println(resAdd);
+        calculator.sub(1,2);
+        int resMul = calculator.mul(3,6);
+        System.out.println(resMul);
+        calculator.div();
     }
 }
